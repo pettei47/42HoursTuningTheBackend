2528e815ed950fb3650d78b1aae85b3f617d0578
pettei47 <kitagawa.teppei.33c@gmail.com>
2022-04-16 06:04:48 +0900

    mineActive,allActive

--- development/backend/src/api.js
+++ development/backend/src/api.js
@@ -389,14 +389,14 @@ const allActive = async (req, res) => {
   const count = recordResult.length;
   const items = Array(count);
 
-  const searchUserQs = 'select * from user where user_id = ?';
-  const searchGroupQs = 'select * from group_info where group_id = ?';
+  const searchUserQs = 'select name from user where user_id = ?';
+  const searchGroupQs = 'select name from group_info where group_id = ?';
   const searchThumbQs =
-    'select * from record_item_file where linked_record_id = ? order by item_id asc limit 1';
+    'select item_id from record_item_file where linked_record_id = ? order by item_id asc limit 1';
   const countQs = 'select count(*) from record_comment where linked_record_id = ?';
-  const searchLastQs = 'select * from record_last_access where user_id = ? and record_id = ?';
+  const searchLastQs = 'select access_time from record_last_access where user_id = ? and record_id = ?';
 
-  for (let i = 0; i < recordResult.length; i++) {
+  for (let i = 0; i < count; i++) {
     const resObj = {
       recordId: null,
       title: '',
@@ -413,10 +413,10 @@ const allActive = async (req, res) => {
 
     const line = recordResult[i];
     mylog(line);
-    const recordId = recordResult[i].record_id;
-    const createdBy = line.created_by;
-    const applicationGroup = line.application_group;
-    const updatedAt = line.updated_at;
+    // const recordId = line.record_id;
+    // const createdBy = line.created_by;
+    // const applicationGroup = line.application_group;
+    // const updatedAt = line.updated_at;
     let createdByName = null;
     let applicationGroupName = null;
     let thumbNailItemId = null;
@@ -453,27 +453,27 @@ const allActive = async (req, res) => {
       }
     }
 
-    resObj.recordId = recordId;
+    resObj.recordId = line.recordId;
     resObj.title = line.title;
-    resObj.applicationGroup = applicationGroup;
+    resObj.applicationGroup = line.applicationGroup;
     resObj.applicationGroupName = applicationGroupName;
-    resObj.createdBy = createdBy;
+    resObj.createdBy = line.createdBy;
     resObj.createdByName = createdByName;
     resObj.createAt = line.created_at;
     resObj.commentCount = commentCount;
     resObj.isUnConfirmed = isUnConfirmed;
     resObj.thumbNailItemId = thumbNailItemId;
-    resObj.updatedAt = updatedAt;
+    resObj.updatedAt = line.updatedAt;
 
     items[i] = resObj;
   }
 
-  const recordCountQs = 'select count(*) from record where status = "open"';
+  // const recordCountQs = 'select count(*) from record where status = "open"';
 
-  const [recordCountResult] = await pool.query(recordCountQs);
-  if (recordCountResult.length === 1) {
-    count = recordCountResult[0]['count(*)'];
-  }
+  // const [recordCountResult] = await pool.query(recordCountQs);
+  // if (recordCountResult.length === 1) {
+  //   count = recordCountResult[0]['count(*)'];
+  // }
 
   res.send({ count: count, items: items });
 };
@@ -611,7 +611,7 @@ const mineActive = async (req, res) => {
     limit = 10;
   }
 
-  const searchRecordQs = `select * from record where created_by = ? and status = "open" order by updated_at desc, record_id asc limit ? offset ?`;
+  const searchRecordQs = `select record_id, created_by, application_group, updated_at from record where created_by = ? and status = "open" order by updated_at desc, record_id asc limit ? offset ?`;
 
   const [recordResult] = await pool.query(searchRecordQs, [user.user_id, limit, offset]);
   mylog(recordResult);
@@ -619,12 +619,12 @@ const mineActive = async (req, res) => {
   const items = Array(recordResult.length);
   let count = 0;
 
-  const searchUserQs = 'select * from user where user_id = ?';
-  const searchGroupQs = 'select * from group_info where group_id = ?';
+  const searchUserQs = 'select name from user where user_id = ?';
+  const searchGroupQs = 'select name from group_info where group_id = ?';
   const searchThumbQs =
-    'select * from record_item_file where linked_record_id = ? order by item_id asc limit 1';
+    'select item_id from record_item_file where linked_record_id = ? order by item_id asc limit 1';
   const countQs = 'select count(*) from record_comment where linked_record_id = ?';
-  const searchLastQs = 'select * from record_last_access where user_id = ? and record_id = ?';
+  const searchLastQs = 'select access_time from record_last_access where user_id = ? and record_id = ?';
 
   for (let i = 0; i < recordResult.length; i++) {
     const resObj = {
