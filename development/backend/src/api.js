cc686ef6dd2edb78f176fed9fa1bdb413e4611b5
pettei47 <kitagawa.teppei.33c@gmail.com>
2022-04-16 06:16:54 +0900

    変数名のミスを修正

--- development/backend/src/api.js
+++ development/backend/src/api.js
@@ -453,17 +453,17 @@ const allActive = async (req, res) => {
       }
     }
 
-    resObj.recordId = line.recordId;
+    resObj.recordId = line.record_id;
     resObj.title = line.title;
-    resObj.applicationGroup = line.applicationGroup;
+    resObj.applicationGroup = line.application_group;
     resObj.applicationGroupName = applicationGroupName;
-    resObj.createdBy = line.createdBy;
+    resObj.createdBy = line.created_by;
     resObj.createdByName = createdByName;
     resObj.createAt = line.created_at;
     resObj.commentCount = commentCount;
     resObj.isUnConfirmed = isUnConfirmed;
     resObj.thumbNailItemId = thumbNailItemId;
-    resObj.updatedAt = line.updatedAt;
+    resObj.updatedAt = line.updated_at;
 
     items[i] = resObj;
   }
@@ -501,17 +501,17 @@ const allClosed = async (req, res) => {
   const [recordResult] = await pool.query(searchRecordQs, [limit, offset]);
   mylog(recordResult);
 
-  const items = Array(recordResult.length);
-  let count = 0;
+  const count = recordResult.length;
+  const items = Array(count);
 
-  const searchUserQs = 'select * from user where user_id = ?';
-  const searchGroupQs = 'select * from group_info where group_id = ?';
+  const searchUserQs = 'select name from user where user_id = ?';
+  const searchGroupQs = 'select name from group_info where group_id = ?';
   const searchThumbQs =
-    'select * from record_item_file where linked_record_id = ? order by item_id asc limit 1';
+    'select item_id from record_item_file where linked_record_id = ? order by item_id asc limit 1';
   const countQs = 'select count(*) from record_comment where linked_record_id = ?';
-  const searchLastQs = 'select * from record_last_access where user_id = ? and record_id = ?';
+  const searchLastQs = 'select access_time from record_last_access where user_id = ? and record_id = ?';
 
-  for (let i = 0; i < recordResult.length; i++) {
+  for (let i = 0; i < count; i++) {
     const resObj = {
       recordId: null,
       title: '',
@@ -528,10 +528,10 @@ const allClosed = async (req, res) => {
 
     const line = recordResult[i];
     mylog(line);
-    const recordId = recordResult[i].record_id;
-    const createdBy = line.created_by;
-    const applicationGroup = line.application_group;
-    const updatedAt = line.updated_at;
+    // const recordId = line.record_id;
+    // const createdBy = line.created_by;
+    // const applicationGroup = line.application_group;
+    // const updatedAt = line.updated_at;
     let createdByName = null;
     let applicationGroupName = null;
     let thumbNailItemId = null;
@@ -568,27 +568,27 @@ const allClosed = async (req, res) => {
       }
     }
 
-    resObj.recordId = recordId;
+    resObj.recordId = line.record_id;
     resObj.title = line.title;
-    resObj.applicationGroup = applicationGroup;
+    resObj.applicationGroup = line.applicatio_group;
     resObj.applicationGroupName = applicationGroupName;
-    resObj.createdBy = createdBy;
+    resObj.createdBy = line.created_by;
     resObj.createdByName = createdByName;
     resObj.createAt = line.created_at;
     resObj.commentCount = commentCount;
     resObj.isUnConfirmed = isUnConfirmed;
     resObj.thumbNailItemId = thumbNailItemId;
-    resObj.updatedAt = updatedAt;
+    resObj.updatedAt = line.updated_at;
 
     items[i] = resObj;
   }
 
-  const recordCountQs = 'select count(*) from record where status = "closed"';
+  // const recordCountQs = 'select count(*) from record where status = "closed"';
 
-  const [recordCountResult] = await pool.query(recordCountQs);
-  if (recordCountResult.length === 1) {
-    count = recordCountResult[0]['count(*)'];
-  }
+  // const [recordCountResult] = await pool.query(recordCountQs);
+  // if (recordCountResult.length === 1) {
+  //   count = recordCountResult[0]['count(*)'];
+  // }
 
   res.send({ count: count, items: items });
 };
