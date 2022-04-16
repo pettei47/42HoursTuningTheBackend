4ac1d536a7e511d203553f5bbe0f946eb5dfd803
pettei47 <kitagawa.teppei.33c@gmail.com>
2022-04-16 20:40:00 +0900

    pt-query-digestが動くように

--- development/build.sh
+++ development/build.sh
@@ -7,16 +7,17 @@
 # web画面の変更を反映したい場合、コメントアウトを外す。
 # (cd ./frontend && npm run build)
 
+test="docker-compose-test.yaml"
+
+docker-compose down --rmi all
+docker-compose -f $test down --rmi all
 
 if [ "$1" != "" ]; then
-    test="docker-compose-test.yaml"
 	echo "slow_query_log mode start"
-	docker-compose -f $test down --rmi all
 	docker-compose -f $test build --no-cache
 	docker-compose -f $test up -d
 else
 	echo "normal test mode start"
-	docker-compose down --rmi all
 	docker-compose build --no-cache
 	docker-compose up -d
 fi
