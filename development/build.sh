e481b24ab6187dd7e1c168f031c4cdda935c8fea
pettei47 <kitagawa.teppei.33c@gmail.com>
2022-04-16 15:49:37 +0900

    test

--- development/build.sh
+++ development/build.sh
@@ -7,6 +7,13 @@
 # web画面の変更を反映したい場合、コメントアウトを外す。
 # (cd ./frontend && npm run build)
 
-docker-compose down --rmi all
-docker-compose build --no-cache
-docker-compose up -d
+
+if [ $1 -eq "test" ]; then
+    test="docker-compose-test.yaml"
+else
+    test=""
+fi
+
+docker-compose $test down --rmi all
+docker-compose $test build --no-cache
+docker-compose $test up -d
